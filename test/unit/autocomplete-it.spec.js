7a69e1935537a2faae975d2fba1750c5fce20876
Daniel Rasmussen <development@dlras.net>
2018-03-20 08:21:54 -0500

    Set up index, constructor, and testing scripts.

---
+++ test/unit/autocomplete-it.spec.js
@@ -0,0 +1,44 @@
+const assert = require('assert');
+const mock = require('mock-require');
+
+let AI;
+
+describe('Unit | AutocompleteIt', () => {
+  beforeEach(() => {
+    AI = require('../../lib/autocomplete-it');
+  });
+
+  it('should reject default constructor', () => {
+    // Arrange
+    const expected = /Values cannot be undefined\./;
+    // Act / Assert
+    assert.throws(() => new AI(), expected);
+  });
+
+  it('should reject null values', () => {
+    // Arrange
+    const values = null;
+    const expected = /Values cannot be null\./;
+    // Act / Assert
+    assert.throws(() => new AI(values), expected);
+  });
+
+  it('should accept no options', () => {
+    // Arrange
+    const values = [];
+    // Act
+    const ai = new AI(values);
+    // Assert
+    assert.deepEqual(ai.options, {});
+  });
+
+  it('should accept options object', () => {
+    // Arrange
+    const values = [];
+    const options = {};
+    // Act
+    const ai = new AI(values, options);
+    // Assert
+    assert.equal(ai.options, options);
+  });
+});
